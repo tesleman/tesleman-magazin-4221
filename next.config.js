eedb099aaff48e00acba96957d7f5247bc88a3c4
tesleman <slavkotesleman@gmail.com>
2021-05-17 18:38:50 +0300

    fix types

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts'],
-  env: {
-    DOMAIN: process.env.DOMAIN,
-    MONGO_URI: process.env.MONGO_URL,
-    SECRET: process.env.SECRET,
-  },
 };
