e316cebb69b0faa7c1b7a371ce569af96ea6ab21
tesleman <slavkotesleman@gmail.com>
2021-05-17 19:22:56 +0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   pageExtensions: ['page.tsx', 'page.ts'],
+  env: {
+    DOMAIN: process.env.DOMAIN,
+    MONGO_URI: process.env.MONGO_URL,
+    SECRET: process.env.SECRET,
+  },
 };
